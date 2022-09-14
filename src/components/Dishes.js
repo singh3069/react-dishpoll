5283692b964d560ee0b4cbcd963859055ce7fb83
Saurabh <raisaurabh265@gmail.com>
2022-09-14 15:09:56 +0530

    now data is showing in console and error msz added

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+
 function Dishes() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://raw.githubusercontent.com/syook/react-dishpoll/main/db.json`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Error: The status is ${response.status}, this in an HTTP error.`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => console.log(actualData))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return <div>Dishes</div>;
 }
 
