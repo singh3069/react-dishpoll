73490bda496896c8b670975ce3b43cfa5c28b707
Saurabh <raisaurabh265@gmail.com>
2022-09-18 19:37:37 +0530

    added msz  if dish are not selected.

--- src/components/Pollresult.js
+++ src/components/Pollresult.js
@@ -1,21 +1,25 @@
 function Pollresult({ selected }) {
   return (
     <div>
-      {selected.map((el) => {
-        return (
-          <div key={el.id} className="flex  flex-col w-60  p-4">
-            <p className="text-center uppercase underline tracking-wider text-lg  pointer-events-none">
-              {el.dishName}
-            </p>
-            <img
-              src={el.image}
-              alt={el.dishName}
-              className="p-3 pointer-events-none"
-            />
-            <p className="pt-1 pointer-events-none">{el.description}</p>
-          </div>
-        );
-      })}
+      {selected.length <= 0 && <p>You have not selected any dish...!!!</p>}
+      <div>
+        {selected.length > 0 &&
+          selected.map((el) => {
+            return (
+              <div key={el.id} className="flex  flex-col w-60  p-4">
+                <p className="text-center uppercase underline tracking-wider text-lg  pointer-events-none">
+                  {el.dishName}
+                </p>
+                <img
+                  src={el.image}
+                  alt={el.dishName}
+                  className="p-3 pointer-events-none"
+                />
+                <p className="pt-1 pointer-events-none">{el.description}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
