53061130c63d489278edf6f49b17334224e7ef6f
Saurabh <raisaurabh265@gmail.com>
2022-09-20 18:06:01 +0530

    styles changed to login page and guest user added

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   }, []);
 
   return (
-    <div className="">
+    <div className="h-full">
       {!isAuthenticated && (
         <LoginForm setIsAuthenticated={setIsAuthenticated} />
       )}
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -30,31 +30,44 @@ function LoginForm({ setIsAuthenticated }) {
   };
 
   return (
-    <div className="bg-slate-300 border-2 border-black rounded-md my-6 mx-auto w-5/6 lg:w-1/4 h-96 justify-center flex items-center flex-col gap-6">
-      <p className="text-4xl tracking-wider">Login</p>
-      <form
-        className="flex flex-col w-10/12 gap-4 items-center"
-        onSubmit={findAuthorisedUser}
-      >
-        <input
-          type="text"
-          name="username"
-          value={userData.username}
-          onChange={onChangeEventHandler}
-          className="border w-3/4 border-black rounded-sm"
-        />
-        <input
-          type="password"
-          name="password"
-          value={userData.password}
-          className="border w-3/4 border-black rounded-sm"
-          onChange={onChangeEventHandler}
-        />
-        <input
-          type="submit"
-          className="border w-1/2  border-black rounded-sm cursor-pointer bg-black text-white p-1"
-        />
-      </form>
+    <div className="flex flex-row w-full h-full">
+      <div className="w-1/3 items-center justify-center flex border-r">
+        <div className="w-full flex items-center justify-end flex-col">
+          <p className="text-5xl font-serif">Welcome to Dish Poll... </p>
+          <p className="text-[11px] pt-3">
+            <strong>Note:</strong> You can use "guest" as username and
+            "guest123" as password.
+          </p>
+        </div>
+      </div>
+      <div className=" border-2 border-black rounded-md my-36 mx-auto w-2/3 lg:w-1/4 h-96 justify-center flex items-center flex-col gap-6 box-border shadow-[0_0px_9px_3px_rgba(0,0,0,0.6)]">
+        <p className="text-4xl tracking-wider">Login</p>
+        <form
+          className="flex flex-col w-10/12 gap-4 items-center"
+          onSubmit={findAuthorisedUser}
+        >
+          <label className="text-left w-3/4">Username</label>
+          <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={onChangeEventHandler}
+            className="border w-3/4 border-black rounded-sm"
+          />
+          <label className="text-left w-3/4">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={userData.password}
+            className="border w-3/4 border-black rounded-sm"
+            onChange={onChangeEventHandler}
+          />
+          <input
+            type="submit"
+            className="border w-1/2  border-black rounded-sm cursor-pointer bg-black text-white p-1"
+          />
+        </form>
+      </div>
     </div>
   );
 }
